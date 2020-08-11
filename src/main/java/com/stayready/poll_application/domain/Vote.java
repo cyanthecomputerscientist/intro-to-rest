e432a1c514e584ed2a58f9087419b0ec1d19b0ef
Dominique <cyanthecomputerscientist@gmail.com>
2020-08-11 13:23:06 -0400

    vote object created

---
+++ src/main/java/com/stayready/poll_application/domain/Vote.java
@@ -0,0 +1,34 @@
+package com.stayready.poll_application.domain;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Vote 
+{
+    @Id
+    @GeneratedValue
+    @Column(name = "Vote_ID")
+    private Long  id;
+
+    @ManyToOne 
+    @JoinColumn(name = "OPTION_ID")
+    private Option option;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setOption(Option option) {
+        this.option = option;
+    }
+    public Option getOption() {
+        return option;
+    }
+    
+}
