f22c48bd7e7dc5d6290647944258d6a83701bcc3
Dominique <cyanthecomputerscientist@gmail.com>
2020-08-12 11:10:39 -0400

    figuring it out

---
+++ src/main/java/com/stayready/poll_application/controller/voteController.java
@@ -0,0 +1,37 @@
+package com.stayready.poll_application.controller;
+
+import javax.inject.Inject;
+
+import com.stayready.poll_application.domain.Vote;
+import com.stayready.poll_application.repositories.voteRepository;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class voteController 
+{
+    @Inject
+    private voteRepository voterepository;
+
+    @RequestMapping(value = "polls/{pollId}/votes", method = RequestMethod.POST)
+    public ResponseEntity<?> createVote(@PathVariable long pollId, @RequestBody Vote vote)
+    {
+        vote = voterepository.save(vote);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(vote.getId()).toUri());
+        return new ResponseEntity<>(responseHeaders, HttpStatus.CREATED);
+    }
+    @RequestMapping(value = "/polls/{pollId}/votes", method = RequestMethod.GET)
+    public Iterable<Vote> getAllVotes(@PathVariable long pollId)
+    {
+        
+    }
+}
